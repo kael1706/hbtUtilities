f9959a3b5a68c945adf09b10e96ea47f125418e7
kaelwebdev <cdcp-1795@hotmail.com>
2021-02-02 07:51:28 -0800

    improving the script. error handling and alias refresh

--- 0x02-hbtsetupHS/hbtsetupHSWS.sh
+++ 0x02-hbtsetupHS/hbtsetupHSWS.sh
@@ -1,25 +1,26 @@
 #!/usr/bin/env bash
+# this is the version without sudo of hbtsetupHS.sh
 echo "======================="
 echo "=========START========="
 cd ~
 echo "moving to:"
 pwd
 {
-  sudo apt install python-pip
+  apt install python-pip
 } || {
   echo "***ERROR***"
   echo -e "error trying to install python-pip, try installing the package manually. Try checking that you have pip installed, for python 2.7. Or try the same command without sudo, i.e:\n apt install python-pip"
   exit 1
 }
 {
-  sudo python2.7 -m pip install mechanize==0.3.7
+  python2.7 -m pip install mechanize==0.3.7
 } || {
   echo "***ERROR***"
   echo -e "error trying to install mechanize v0.3.7, try installing the package manually. Try checking that you have pip installed, for python 2.7. Or try the same command without sudo, i.e:\n python2.7 -m pip install mechanize==0.3.7"
   exit 1
 }
 {
-  sudo python2.7 -m pip install beautifulsoup4==4.6.3
+  python2.7 -m pip install beautifulsoup4==4.6.3
 } || {
   echo "***ERROR***"
   echo -e "error trying to install beautifulsoup4 v4.6.3, try installing the package manually. Try checking that you have pip installed, for python 2.7. Or try the same command without sudo, i.e:\n python2.7 -m pip install beautifulsoup4==4.6.3"
